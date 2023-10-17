c736109b7906b2a07c0f30e35493d67aa20a5e17
Joana Parente <jrparente@gmail.com>
2023-10-17 11:37:36 +0100

    Improved project Info

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,16 +5,16 @@ export default [
     isFeatured: true,
     isPublicRepository: true,
     iscasestudy: true,
+    role: "Full-stack Developer",
     date: "2023-10-15",
     name: "Farol Discover 🗺️",
-    tagline: "A Travel-focused website with dynamic content and a custom CMS",
+    tagline: "A Travel-focused Website with Dynamic Content and a Custom CMS",
     problemStatement:
       "Creating a user-friendly travel website that allows easy content management and dynamic content fetching.",
-    role: "Full-stack Developer",
     shortDescription:
-      "Farol Discover is a travel-focused website with a custom CMS, dynamic content fetching, and a responsive design, built using React and Next.js.",
+      "Farol Discover is a travel-focused website offering a custom CMS for dynamic content management, and a responsive design, built using React and Next.js.",
     description:
-      "Farol Discover is a travel-centric website, uniquely designed to be user-friendly. It features a customized Sanity Studio CMS that allows easy content management across various pages, including the homepage, about page, and contact page. The CMS also enables the client to manage tour programs and testimonials dynamically.",
+      "Farol Discover is a travel-centric website designed with user-friendliness at its core. It features a custom-built Sanity Studio CMS that allows easy and real-time content management across multiple pages, including the homepage, about us, and contact pages. This CMS flexibility extends to managing tour programs and testimonials dynamically.",
     image: "/farol-discover-clipped.png",
     links: {
       github: "https://github.com/jrparente/farol-discover",
@@ -29,20 +29,20 @@ export default [
       "Shadcnui",
     ],
     features: [
-      "Custom Sanity Studio: Allows the client to manage content for multiple pages and create tour programs and testimonials.",
-      "Contact Form: Features a fully functional contact form for customer inquiries.",
-      "Dynamic Content Fetching: Utilizes GROQ queries to fetch content dynamically to display on the frontend.",
-      "Responsive Design: Built with Tailwind CSS and Shadcnui for a fully responsive user interface.",
+      "Custom Sanity Studio: Enables the client to manage content across various pages, as well as dynamically create and update tour programs and testimonials.",
+      "Contact Form: Incorporates a fully functional contact form for customer inquiries.",
+      "Dynamic Content Fetching: Leverages GROQ queries to dynamically fetch and display content from the backend.",
+      "Responsive Design: Utilizes Tailwind CSS and Shadcnui for a fully responsive and intuitive user interface.",
     ],
     codeHighlights: [
-      "Sanity Studio: Created custom schemas in Sanity Studio for real-time updates and content management.",
-      "GROQ: Utilized GROQ queries for dynamic content fetching from Sanity Studio.",
-      "Tailwind and Shadcnui: Leveraged Tailwind CSS and Shadcnui for styling and responsive design.",
+      "Sanity Studio: Developed custom schemas in Sanity Studio for real-time content updates and versatile content management.",
+      "GROQ: Utilized GROQ queries to dynamically fetch content from Sanity Studio, thereby providing a real-time user experience.",
+      "Tailwind and Shadcnui: Employed Tailwind CSS and Shadcnui for styling, ensuring a responsive and visually appealing design.",
     ],
     challengesAndLearnings: [
-      "CMS Integration: Learned how to integrate a headless CMS (Sanity Studio) into a React and Next.js application.",
-      "Dynamic Data Fetching: Gained experience in using GROQ for querying data dynamically.",
-      "UI/UX Design: Focused on creating a seamless and intuitive user interface.",
+      "CMS Integration: Acquired hands-on experience in integrating a headless CMS (Sanity Studio) with a React and Next.js application. This was my first venture into using Sanity, which involved learning to set up the studio, create schemas, and manage content dynamically.",
+      "Dynamic Data Fetching: Gained valuable insights into using GROQ for querying data dynamically from Sanity Studio, a crucial part of creating a real-time, dynamic website.",
+      "UI/UX Design: Invested significant effort in creating a seamless and intuitive user interface, focusing on UX principles to enhance user engagement.",
     ],
     results: [],
   },
@@ -52,8 +52,11 @@ export default [
     isFeatured: false,
     iscasestudy: true,
     isPublicRepository: false,
+    role: "Full-stack Developer",
     date: "2023-09-15",
     name: "AI Business Genie 🤖",
+    tagline:
+      "Empowering Businesses with AI: Automate Content and Plan Strategically",
     shortDescription:
       "An AI-powered business and content creation assistant designed to streamline daily tasks for professionals.",
     description:
@@ -72,21 +75,29 @@ export default [
       "Shadcnui",
       "Clerk Auth",
       "Prisma",
+      "AI Prompt Engineering",
+      "Stripe Payments",
     ],
     features: [
       "Content Generation: Automatically generates high-quality blog posts, social media captions, and marketing copies.",
       "Business Planning: Assists in crafting comprehensive business plans and financial projections.",
       "User Authentication: Secure login and registration functionality through Clerk Auth.",
-      "Real-time Database: Utilizes Prisma for real-time database management.",
+      "Database: Utilizes Prisma for CRUD operations, efficiently managing user data and content generations.",
+      "Free Tier & Payment Logic: Features a free tier that allows up to 5 free content generations. Beyond that, payment is required. This limitation is checked prior to making API requests to OpenAI, and the user's generation count is stored in Prisma for users who are not subscribed.",
     ],
     codeHighlights: [
       "AI Integration: Integrated OpenAI for automated content creation.",
       "Authentication: Used Clerk Auth for secure and efficient user authentication.",
       "Database: Implemented Prisma for database management.",
+      "Payment Integration: Successfully integrated Stripe API for payment processing, marking my first experience in handling real-time financial transactions within an app.",
+      "AI Prompt Engineering: Utilized prompt engineering techniques to improve the accuracy and relevance of AI-generated content, customizing the OpenAI GPT model prompts for specialized business use-cases.",
     ],
     challengesAndLearnings: [
-      "Function Timeouts: Faced challenges with Vercel's function timeouts during OpenAI fetch requests.",
-      "Compatibility Issues: Encountered compatibility issues between Clerk Auth, Prisma, and edge functions.",
+      "Vercel Timeouts on API Fetch: Encountering a persistent issue with Vercel's 10-second function timeout limit in the free tier, which is affecting fetch requests to OpenAI's API. The timeouts make the app appear unresponsive and this problem is unique to the deployed version; no such issue occurs in the local environment. Currently exploring alternative deployment solutions and optimization techniques to resolve this.",
+      "Edge Function Compatibility: Initially explored Vercel's edge functions coupled with AI SDK streaming as a workaround to bypass the function timeout limitations. However, found that edge functions are not compatible with Clerk Auth and Prisma, creating a dilemma. The edge functions could resolve the timeout issue but introduce security vulnerabilities due to the inability to validate users and enforce request limits effectively.",
+      "Shadcn UI Customization: Opted for Shadcn UI over other UI libraries for its unique installation setup. Components are installed directly into the components/ui folder, providing full access to the component code. This flexibility allows for seamless customization, unlike the rigid, pre-formatted components found in libraries like Material UI.",
+      "Stripe Payment Integration: Successfully implemented Stripe for payment processing, which was a first-time experience for me. The integration is fully functional, allowing for real-time financial transactions.",
+      "AI Prompt Engineering Challenges: Navigated through the complexities of AI prompt engineering to fine-tune the performance of AI-generated content. This was crucial for ensuring that the OpenAI GPT model outputs were aligned with the specialized business use-cases of the platform.",
     ],
   },
   {
@@ -95,6 +106,7 @@ export default [
     isFeatured: true,
     iscasestudy: true,
     isPublicRepository: true,
+    role: "Full-stack Developer",
     date: "2023-10-15",
     name: "Library Management Dashboard 📚",
     shortDescription:
@@ -124,7 +136,6 @@ export default [
       "Responsive Design: The UI is fully responsive and adapts to various screen sizes.",
     ],
     codeHighlights: [
-      "State Management: Utilized React hooks (useState, useEffect) and custom hooks for state management.",
       "Backend Integration: Integrated Supabase for real-time database updates and user authentication.",
       "Server-side Rendering: Used Next.js for server-side rendering in the desktop version, improving performance and SEO.",
       "Mobile Development: Implemented React Native for the mobile app versions.",
@@ -133,8 +144,8 @@ export default [
     ],
     challengesAndLearnings: [
       "Search and Filter: Implementing real-time search and filter functionalities.",
-      "State Management: Managing state across various components and contexts.",
       "Backend Learning: Learning to work with Supabase for backend functionalities.",
+      "Tailwind CSS Learning: This was my first experience with Tailwind CSS. Despite an initial learning curve, I found it aligned well with my preference for utility-first CSS, enhancing my development speed.",
       "Mobile Adaptation: Adapting the desktop version into a mobile app using React Native.",
     ],
   },
@@ -144,6 +155,7 @@ export default [
     isFeatured: false,
     iscasestudy: true,
     isPublicRepository: false,
+    role: "Full-stack Developer",
     date: "2023-08-15",
     name: "AI Travel Planner ✈️",
     shortDescription:
@@ -189,6 +201,7 @@ export default [
     isFeatured: false,
     iscasestudy: false,
     isPublicRepository: true,
+    role: "Front-end Developer",
     date: "2023-10-15",
     name: "Personal portfolio website 📝",
     shortDescription:
@@ -208,6 +221,7 @@ export default [
     isFeatured: false,
     isPublicRepository: true,
     iscasestudy: false,
+    role: "Front-end Developer",
     date: "2023-01-15",
     name: "Simple Dinners Generator 🍽️",
     image: "./simple-dinners.png",
