20abc82547f91b4193f54fda124c052119aaa351
Joana Parente <jrparente@gmail.com>
2023-10-26 13:52:47 +0100

    .

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -4,7 +4,7 @@ import Button from "../ui/Button";
 
 function HeroSection() {
   return (
-    <section className="section-hero min-h-screen flex flex-col gap-16 mt-24 pt-24">
+    <section className="section min-h-screen flex flex-col gap-16 mt-24 pt-24">
       <div className="max-w-screen-xl mx-auto flex-1 flex flex-col justify-center md:flex-row md:items-center gap-8  px-5 xs:px-10 sm:px-12 md:px-24 ">
         <article className="w-full">
           <p className="text-primary-500 font-mono">Hi, my name is</p>
