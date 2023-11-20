e3ee50f5701fd3987319b5b4e07a300c233a0690
Joana Parente <jrparente@gmail.com>
2023-11-20 12:22:50 +0000

    re-styled skill bar

--- src/components/SkillBar/SkillBar.tsx
+++ src/components/SkillBar/SkillBar.tsx
@@ -1,26 +1,42 @@
-import { Code, Globe, Feather, DollarSign, Cpu } from "lucide-react";
+import { Code, Globe, Feather, DollarSign, Cpu, Database } from "lucide-react";
 
 function SkillBar() {
   return (
     <div className="mt-auto flex flex-col lg:flex-row items-center justify-center lg:gap-32 py-5 bg-gray-200 md:mt-15 lg:mt-0 px-5 xs:px-10 sm:px-12 md:px-24 text-sm lg:text-base uppercase font-semibold">
       <div className="flex flex-row items-center my-2 lg:my-0">
-        <Code className="w-5 h-5" />
+        <div className="bg-primary-500/10 p-2 rounded-md">
+          <Code className="w-5 h-5 text-primary-500" />
+        </div>
         <span className="ml-2">HTML, CSS, JavaScript</span>
       </div>
       <div className="flex flex-row items-center my-2 lg:my-0">
-        <Globe className="w-5 h-5" />
-        <span className="ml-2">React, Next.js</span>
+        <div className="bg-gray-500/10 p-2 rounded-md">
+          <img src="./react-logo.svg" alt="React Logo" className="w-5 h-5 " />
+        </div>
+        <span className="ml-2">React</span>
       </div>
+      {/* <div className="flex flex-row items-center my-2 lg:my-0">
+        <div className="bg-primary-500/10 p-2 rounded-md">
+          <Database className="w-5 h-5 text-primary-500" />
+        </div>
+        <span className="ml-2">SQL</span>
+      </div> */}
       <div className="flex flex-row items-center my-2 lg:my-0">
-        <Feather className="w-5 h-5" />
+        <div className="bg-primary-500/10 p-2 rounded-md">
+          <Feather className="w-5 h-5 text-primary-500" />
+        </div>
         <span className="ml-2">UI/UX Design</span>
       </div>
       <div className="flex flex-row items-center my-2 lg:my-0">
-        <DollarSign className="w-5 h-5" />
+        <div className="bg-primary-500/10 p-2 rounded-md">
+          <DollarSign className="w-5 h-5 text-primary-500" />
+        </div>
         <span className="ml-2">Digital Marketing</span>
       </div>
       <div className="flex flex-row items-center my-2 lg:my-0">
-        <Cpu className="w-5 h-5" />
+        <div className="bg-primary-500/10 p-2 rounded-md">
+          <Cpu className="w-5 h-5 text-primary-500" />
+        </div>
         <span className="ml-2">Artificial Intelligence</span>
       </div>
     </div>
